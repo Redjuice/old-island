475a5ddb2c34e50e7cacfd19f0dc21e1770ac915
wangheng <wangheng@vchangyi.com>
2021-11-11 15:38:07 +0800

    配置vant以及按需加载

--- h5-vue3-vite-js/vite.config.js
+++ h5-vue3-vite-js/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import styleImport from "vite-plugin-style-import";
+import { devPort, open } from "./src/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    styleImport({
+      libs: [
+        {
+          libraryName: "vant",
+          esModule: true,
+          resolveStyle: (name) => `vant/es/${name}/style`,
+        },
+      ],
+    }),
+  ],
+  server: {
+    port: devPort,
+    open,
+  },
+});
